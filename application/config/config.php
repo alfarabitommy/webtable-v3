da1bfc25fd2deb8bc0f64495fcfd204b12e496ba
Tommy Alfarabi <tommy.alfarabi@gmail.com>
2026-09-14 18:45:06 +0700

    https on

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (string) (getenv('APP_BASE_URL') ?: 'http://synapserent.com/');
+$config['base_url'] = (string) (getenv('APP_BASE_URL') ?: 'https://synapserent.com/');
 // $config['base_url'] = (string) (getenv('APP_BASE_URL') ?: 'http://synapse.test/');
 // $config['base_url'] = (string) (getenv('APP_BASE_URL') ?: 'https://synapse.cml-indonesia.com/');
 
